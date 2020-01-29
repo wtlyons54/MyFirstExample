0784078abe0881598b8f181dbe9e919e05d84f2a
Ramsfield <mgriffith004@mail.csuchico.edu>
2020-01-29 11:34:47 -0800

    Added the ability to add two numbers together

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 
 using std::cin;
 using std::cout;
+using std::endl;
 
 int main()
 {
     cout<<"Hi, please enter two numbers: ";
+
+    int x,y;
+
+    cin >> x >> y;
+    cout << x + y << endl;
 
     return 0;
 }
